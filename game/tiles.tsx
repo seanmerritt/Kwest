bc6602452e19a3ae4450a6c165c0bc0fafd23c9a
Sean Merritt <seanmerritt10@gmail.com>
2021-03-16 09:23:58 -0600

    added the tiles.tsx so hopefully it works

---
+++ game/tiles.tsx
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles" tilewidth="129" tileheight="129" tilecount="37" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="21">
+  <image width="128" height="128" source="png/Tiles/1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="png/Tiles/2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="png/Tiles/3.png"/>
+ </tile>
+ <tile id="24">
+  <image width="129" height="129" source="png/Tiles/4.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="png/Tiles/5.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="png/Tiles/6.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="png/Tiles/7.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="png/Tiles/8.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="png/Tiles/9.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="png/Tiles/10.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="png/Tiles/11.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="png/Tiles/12.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="93" source="png/Tiles/13.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="93" source="png/Tiles/14.png"/>
+ </tile>
+ <tile id="35">
+  <image width="128" height="93" source="png/Tiles/15.png"/>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="png/Tiles/16.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="99" source="png/Tiles/17.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="png/Tiles/18.png"/>
+ </tile>
+ <tile id="39">
+  <image source="OneDrive/Documents/BYU/Kwest/game/png/Object/Mushroom_1.png"/>
+ </tile>
+ <tile id="40">
+  <image source="OneDrive/Documents/BYU/Buildings expansion/preview.png"/>
+ </tile>
+ <tile id="41">
+  <image source="OneDrive/Documents/BYU/Request pack/Tiles/metalMid.png"/>
+ </tile>
+ <tile id="42">
+  <image width="47" height="47" source="png/Tiles/Extra animations and enemies/Alien sprites/alienBeige_badge1.png"/>
+ </tile>
+ <tile id="43">
+  <image width="66" height="92" source="png/Tiles/Extra animations and enemies/Alien sprites/alienPink.png"/>
+ </tile>
+ <tile id="44">
+  <image source="OneDrive/Documents/BYU/Request pack/Tiles/stoneCaveUL.png"/>
+ </tile>
+ <tile id="45">
+  <image source="OneDrive/Documents/BYU/Request pack/Tiles/stoneCaveUR.png"/>
+ </tile>
+ <tile id="46">
+  <image source="OneDrive/Documents/BYU/Request pack/Tiles/metalPlatform.png"/>
+ </tile>
+ <tile id="47">
+  <image source="OneDrive/Documents/BYU/Mushroom expansion/Backgrounds/bg_shroom.png"/>
+ </tile>
+ <tile id="48">
+  <image source="OneDrive/Documents/BYU/Buildings expansion/Tiles/houseBeige.png"/>
+ </tile>
+ <tile id="49">
+  <image source="OneDrive/Documents/BYU/Base pack/Tiles/ladder_mid.png"/>
+ </tile>
+ <tile id="50">
+  <image source="OneDrive/Documents/BYU/Base pack/Tiles/ladder_top.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="png/Tiles/ladder_top_128x128.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="png/Tiles/ladder_mid_128x128.png"/>
+ </tile>
+ <tile id="53">
+  <image width="70" height="70" source="png/Tiles/Base pack/Tiles/boxCoin.png"/>
+ </tile>
+ <tile id="54">
+  <image width="70" height="70" source="png/Tiles/Request pack/Tiles/raygun.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="png/Tiles/back.png"/>
+ </tile>
+ <tile id="56">
+  <image width="70" height="70" source="png/Tiles/Mushroom expansion/PNG/tinyShroom_red.png"/>
+ </tile>
+ <tile id="57">
+  <image width="70" height="70" source="png/Tiles/Mushroom expansion/PNG/tallShroom_red.png"/>
+ </tile>
+</tileset>
